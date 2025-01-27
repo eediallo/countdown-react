daa458a769a83a41a0a809d63f01f7a12925106f
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-27 13:18:34 +0000

    adds  static challenges

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
+import TimerChallenge from "./components/TimerChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" timer={1} />
+        <TimerChallenge title="Not Easy" timer={5} />
+        <TimerChallenge title="Getting Tough" timer={10} />
+        <TimerChallenge title="PROS ONLY" timer={15} />
+      </div>
     </>
   );
 }
